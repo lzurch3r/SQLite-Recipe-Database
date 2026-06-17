2b173b1ee589aa9e10a5a84ef90551da6fc9b568
Lev Zurcher <lazurcher5@gmail.com>
2026-06-16 18:26:50 -0600

    Update recipes_select.py
    Program now displays instruction steps for each recipe in RECIPES table

--- recipes_select.py
+++ recipes_select.py
@@ -3,16 +3,19 @@ import sqlite3
 database = 'database/recipes.db'
 select_recipes = "SELECT * from RECIPES"
 select_ingredients = "SELECT * from INGREDIENTS"
-select_instructions = "SELECT * from INSTRUCTIONS"
+#recipe_id = 0
+#select_instructions = "SELECT * from INSTRUCTIONS WHERE recipe_id=?", (recipe_id,)
 
 try:
     with sqlite3.connect(database) as conn:
-        cursor = conn.cursor()
+        cursor_recipes = conn.cursor()
+        cursor_instructions = conn.cursor()
 
-        cursor.execute(select_recipes)
+        cursor_recipes.execute(select_recipes)
 
-        rows = cursor.fetchall()
+        rows = cursor_recipes.fetchall()
 
+        # Display all of the recipes in the list
         for row in rows:
             print("Recipe ID:", row[0])
             print("Title:", row[1])
@@ -21,16 +24,18 @@ try:
             print("Prep time:", row[4], "minutes")
             print("Cook time:", row[5], "minutes")
             print("Servings:", row[6])
+            
+            # Move on to displaying the steps for each recipe
+            recipe_id = row[0]
+            count_query = cursor_instructions.execute("SELECT Count() from INSTRUCTIONS WHERE recipe_id=?", (recipe_id,))
+            row_count = count_query.fetchone()[0]
+            # If there are no steps, skip the process of querying more data
+            if (row_count > 0):
+                ins_rows = cursor_instructions.execute("SELECT * from INSTRUCTIONS WHERE recipe_id=?", (recipe_id,))
+                for ins_row in ins_rows:
+                    print("  Step", ins_row[0])
+                    print("    ", ins_row[1])
             print("")
-
-        cursor.execute(select_instructions)
-
-        rows = cursor.fetchall()
-
-        for row in rows:
-            print("Recipe ID:", row[2])
-            print("Step:", row[0])
-            print(row[1])
 
 except sqlite3.OperationalError as e:
     print(e)
