960fc23b6b0a84635169dc4df34d0d1451e0ec24
Lev Zurcher <lazurcher5@gmail.com>
2026-06-17 16:55:31 -0600

    Added recipes_update.py
    Python code added to update an item in the RECIPES table.

--- recipes_update.py
+++ recipes_update.py
@@ -1,4 +1,19 @@
 import sqlite3
 
-database = 'database/recipe.db'
-update_recipes
+database = 'database/recipes.db'
+update_recipes = "UPDATE RECIPES SET\
+    title = 'Mangy Oils',\
+    cuisine = 'flibbity',\
+    tag = 'Harry Potter potions',\
+    prep_time = '2000',\
+    cook_time = '20',\
+    servings = 1\
+    WHERE recipe_id = 5"
+
+try:
+    with sqlite3.connect(database) as conn:
+        cur = conn.cursor()
+        cur.execute(update_recipes)
+        conn.commit()
+except sqlite3.OperationalError as e:
+    print(e)
